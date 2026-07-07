6eb7b4d5fa98d2c5316f04397d5a0364b3d9d7b3
alokrk04 <alokrk04@gmail.com>
2026-07-07 18:25:52 +0530

    address added

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter
 
 api_router = APIRouter()
 
-from app.api.auth import router as auth_router
+from app.api.compat import router as compat_router
 from app.api.public import router as public_router
 from app.api.admin import router as admin_router
+from app.api.auth import router as auth_router
 
-api_router.include_router(auth_router, tags=["Auth"])
+api_router.include_router(compat_router, tags=["Compat"])
 api_router.include_router(public_router, tags=["Public"])
 api_router.include_router(admin_router, prefix="/admin", tags=["Admin"])
+api_router.include_router(auth_router, tags=["Auth"])
