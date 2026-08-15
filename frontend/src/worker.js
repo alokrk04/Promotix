b4bf01569d2c452edb6036675db55973069a24ae
alokrk04 <alokrk04@gmail.com>
2026-08-15 15:54:32 +0530

    Updated frontend

--- frontend/src/worker.js
+++ frontend/src/worker.js
@@ -16,10 +16,6 @@ export default {
       return response
     }
 
-    if (url.pathname === '/login') {
-      return env.ASSETS.fetch(new Request(new URL('/login.html', url)))
-    }
-
     return env.ASSETS.fetch(request)
   },
 }
